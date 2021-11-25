e529d191602516678cec59f021291a466ebcc9f3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-11-25 11:15:30 -0300

    :tada: :electric_plug:  new Components

---
+++ meu-projeto/src/components/Imc.js
@@ -0,0 +1,25 @@
+function Imc({ altura, peso }) {
+    function imc(IMC) {
+        if (IMC < 18.5) {
+            return 'Abaixo do peso'
+        } else if (IMC >= 18.5 && IMC <= 24.9) {
+            return 'Peso normal'
+        } else if (IMC >= 25 && IMC <= 29.9) {
+            return 'Sobrepeso'
+        } else if (IMC >= 30 && IMC <= 34.9) {
+            return 'Obesidade grau I'
+        } else if (IMC >= 35 && IMC <= 39.9) {
+            return 'Obesidade grau II'
+        } else if (IMC >= 40) {
+            return 'Obesidade grau III'
+        }
+    }
+
+    return (
+        <>
+            <p id="imc">{imc(peso / (altura * altura))}</p>    
+        </>
+    )
+}
+
+export default Imc;
---
+++ meu-projeto/src/components/Informacoes.js
@@ -0,0 +1,30 @@
+function Informacoes({ idade, altura, peso }) {
+
+    return (
+        <>
+            <p>Idade: {idade} anos</p>
+            <p>Altura: {altura}m</p>
+            <p>Peso: {peso}kg</p>
+
+        </>
+    )
+}
+
+
+// Forma errada
+
+// function Informacoes(props) {
+
+//     console.log(props);
+
+//     return (
+//         <>
+//             <p>Sua idade é {props.idade} anos</p>
+//             <p>Sua altura atual é {props.altura}m</p>
+//             <p>Seu peso é {props.peso}kg</p> 
+
+//         </>
+//     )
+// }
+
+export default Informacoes;
