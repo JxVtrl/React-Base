6a8bde1c0b0a58393223b6fa36d6e061f4cdd063
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-11-25 11:15:14 -0300

    :sparkles: App Update

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,43 +1,30 @@
 import './App.css';
+import Informacoes from './components/Informacoes.js';
+import Imc from './components/Imc.js';
 
 function App() {
+  const url = 'https://via.placeholder.com/150'
   const myInfo = {
-    nome: 'João',
+    nome: 'João'.toUpperCase(),
     idade: 20,
     altura: 1.84,
     peso: 72,
   }
-  const newName = myInfo.nome.toUpperCase() 
-
-  function imc(IMC) {
-    if (IMC < 18.5) {
-      return 'Abaixo do peso'
-    } else if (IMC >= 18.5 && IMC <= 24.9) {
-      return 'Peso normal'
-    } else if (IMC >= 25 && IMC <= 29.9) {
-      return 'Sobrepeso'
-    } else if (IMC >= 30 && IMC <= 34.9) {
-      return 'Obesidade grau I'
-    } else if (IMC >= 35 && IMC <= 39.9) {
-      return 'Obesidade grau II'
-    } else if (IMC >= 40) {
-      return 'Obesidade grau III'
-    }
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
 
   return (
     <div className="App">
-      <h1>Olá, { newName }!</h1>
+      <h1>Olá, { myInfo.nome }!</h1>
       <p>Recolhemos suas informações, aqui estão.</p>
-      
-      <p>Sua idade é {myInfo.idade} anos</p>
-      <p>Sua altura atual é {myInfo.altura}m</p>
-      <p>Seu peso é {myInfo.peso}kg</p>
 
-      <p>Seu IMC é {imc(myInfo.peso / (myInfo.altura * myInfo.altura))}</p>
+      <Informacoes
+        nome={myInfo.nome}
+        idade={myInfo.idade}
+        altura={myInfo.altura}
+        peso={myInfo.peso} />
+      
+      <Imc
+        altura={myInfo.altura}
+        peso={myInfo.peso} />
 
       <img src={url} alt="Img" />
     </div>
